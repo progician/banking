90c4b3589be994eb6534c4c0a9a0275371140b12
Gyula Gubacsi <gyula.gubacsi@gmail.com>
2024-03-08 19:08:02 +0000

    test: Expect account balance be empty after created

--- tests/functional.rs
+++ tests/functional.rs
@@ -2,6 +2,7 @@ use assert2::assert;
 use rstest::{fixture, rstest};
 use rocket::local::blocking::Client;
 use rocket::http::Status;
+use rocket::serde::Deserialize;
 
 #[path = "../src/main.rs"]
 mod main;
@@ -9,14 +10,55 @@ mod main;
 use main::rocket;
 
 
+struct TestClient {
+    client: Client,
+}
+
+#[derive(Deserialize)]
+struct AccountIDResponse {
+    account_id: AccountID,
+}
+
+
+#[derive(Deserialize)]
+struct BalanceResponse {
+    account_id: AccountID,
+    balance_amount: u64,
+    balance_currency: String,
+}
+
+
+type AccountID = u64;
+
+
+impl TestClient {
+    fn new() -> Self {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        TestClient { client }
+    }
+
+    fn create_account(&self) -> AccountID {
+        let response = self.client.get("/api/create").dispatch();
+        assert!(response.status() == Status::Ok);
+        response.into_json::<AccountIDResponse>().unwrap().account_id
+    }
+
+    fn balance_of(&self, account_id: AccountID) -> u64 {
+        let response = self.client.get(format!("/api/balance/{}", account_id)).dispatch();
+        assert!(response.status() == Status::Ok);
+        response.into_json::<BalanceResponse>().unwrap().balance_amount
+    }
+}
+
+
 #[fixture]
-fn client() -> Client {
-    Client::tracked(rocket()).expect("valid rocket instance")
+fn client() -> TestClient {
+    TestClient::new()
 }
 
 #[rstest]
-fn newly_created_accounts_are_empty(client: Client) {
-    let response = client.get("/").dispatch();
-    assert!(response.status() == Status::Ok);
-    assert!(response.into_string().unwrap() == "Hello, World!");
+fn newly_created_accounts_are_empty(client: TestClient) {
+    let account_id = client.create_account();
+    let account_balance = client.balance_of(account_id);
+    assert!(account_balance == 0);
 }
